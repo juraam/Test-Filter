4715e0d011cbac14d78bf1dfd927aef7e989f0cc
Yuriy Moshkov <juramoshkov@gmail.com>
2019-02-22 15:56:38 +0300

    Initial Commit

---
+++ testfilters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testfilters
+//
+//  Created by Jura Moshkov on 22/02/2019.
+//  Copyright © 2019 Jura Moshkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testfilters/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testfilters
+//
+//  Created by Jura Moshkov on 22/02/2019.
+//  Copyright © 2019 Jura Moshkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
